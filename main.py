e8290c2409df9a620bb519fb2895933da066a743
soul0101 <53980340+soul0101@users.noreply.github.com>
2022-10-15 02:48:01 +0530

    fix bug

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from signver.detector import Detector
 DIR = os.path.dirname(__file__)
 DETECTOR_MODEL_PATH = "models/detector/small"
 
-detector = Detector()
-detection_model = detector.load(os.path.join(DIR, DETECTOR_MODEL_PATH))
+detection_model = Detector()
+detection_model.load(os.path.join(DIR, DETECTOR_MODEL_PATH))
 
 def detect(input_tensor):
     return detection_model.detect(input_tensor)
